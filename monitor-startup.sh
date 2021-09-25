a9d11e1cd5f46ab7d4d4d87248284a5c03c58a25
Night-Raider <37450990+Night-Raider@users.noreply.github.com>
2021-09-24 23:25:22 -0500

    Delete monitor-startup.sh

--- monitor-startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pkill -f libgpiod_pulsein
-exec -a Terrarium-HcsrHumidifier python3 /var/lib/rpi-terrarium-controller/sensors/HcsrHumidifier.py &
-exec -a Terrarium-Htu31d python3 /var/lib/rpi-terrarium-controller/sensors/Htu31d.py &
