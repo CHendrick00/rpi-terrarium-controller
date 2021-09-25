f54e62b1f6a48a6b183c58ff6f4cba3dfe940636
Night-Raider <37450990+Night-Raider@users.noreply.github.com>
2021-09-24 19:12:25 -0500

    Update PiInstall.sh

--- PiInstall.sh
+++ PiInstall.sh
@@ -7,7 +7,7 @@ FILE=./tmp
 if [ ! -f "$FILE" ]; then
 
   # create tmp file when done to avoid running this section on second run
-  touch tmp
+  sudo touch tmp
 
   sudo apt-get update
   sudo apt-get upgrade
@@ -16,13 +16,21 @@ if [ ! -f "$FILE" ]; then
 
   sudo pip3 install --upgrade adafruit-python-shell
   wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+  echo "The system will restart after completing the following step."
+  echo "Please run this script one more time after the device restarts to finish installation."
+  echo "To acknowledge and continue, press any key..."
+  read -n 1 -s
   sudo python3 raspi-blinka.py
 
 fi
 
 # ensure CircuitPython install was successful
-sudo wget https://raw.githubusercontent.com/Night-Raider/rpi-terrarium-controller/main/blinkatest.py
+cd ~
+sudo rm ./tmp
+sudo rm ./raspi-blinka.py
+cd ~/rpi-terrarium-controller
 python3 blinkatest.py
+sudo rm ./blinkatest.py
 pip3 install --upgrade adafruit_blinka
 
 # install required libraries
@@ -31,18 +39,19 @@ sudo pip3 install adafruit-circuitpython-htu31d
 sudo pip3 install discord-webhook
 sudo pip3 install influxdb
 
-# download sensor files
-wget https://raw.githubusercontent.com/Night-Raider/rpi-terrarium-controller/main/HcsrHumidifier.py
+cd ~/rpi-terrarium-controller
+chmod u+x monitor-startup.sh
+mv ./terrarium-monitor.service /etc/systemd/system/terrarium-monitor.service
+sudo systemctl daemon-reload
+sudo systemctl enable terrarium-monitor.service
+sudo systemctl start terrarium-monitor.service
 
-sudo rm ./tmp
 
-echo "ATTENTION: Before running, you MUST open each hardware-specific file and change the parameters listed at the top to fit your specific situation if needed."
-echo "To do this, use the command <sudo nano \"filename\">, change the values as needed, then save with ctrl+s, ctrl+x"
-echo "To view all files installed, use the command <ls>"
-echo "To delete an unneeded file, use command <rm ./filename>"
-echo "To run a script automatically at startup, run the command <crontab -e>, select option 1, then at the bottom of the file add the line:"
-echo "<@reboot python3 ~/filename.py>, replacing filename with the name of the file to be ran"
-echo "Additional files can be added to run on reboot simply by adding another line and changing the filename to match"
+echo "ATTENTION: Before finishing installation, you MUST open each hardware-specific file you plan to use and change the parameters listed at the top to fit your specific situation as needed."
+echo "To do this, use the command <sudo nano ~/rpi-terrarium-controller/sensors/FILENAME>, change the values as needed, then save with ctrl+s, ctrl+x"
+echo "To view all sensor files installed, use the commands <cd ~/rpi-terrarium-controller/sensors>, then <ls>"
+echo "To run a sensor on startup, run the command <sudo nano ~/rpi-terrarium-controller/monitor-startup.sh> and add another line using the filename of the sensor you plan to use"
+echo "After adding a sensor to the startup file, simply reboot or run the command <sudo systemctl restart terrarium-monitor>"
 
 echo "All of these instructions can be found again in the README on my Github repository"
 echo "Note: do not include the <> characters in the commands above"
