802f9cbda07b6e032b232c324332b965807730d2
Night-Raider <37450990+Night-Raider@users.noreply.github.com>
2021-10-05 20:24:31 -0500

    Update Uninstall.sh

--- Uninstall.sh
+++ Uninstall.sh
@@ -3,5 +3,5 @@ sudo systemctl disable terrarium-*.service
 sudo systemctl stop terrarium-monitor.target
 cd ~
 sudo rm -r "$(pwd)/rpi-terrarium-controller" /var/lib/rpi-terrarium-controller
-find . -wholename "/etc/systemd/system/terrarium-*" -exec sudo rm -r '{}' \;
+find /etc/systemd/system/ -name "terrarium-*" -exec sudo rm -r {} \;
 sudo systemctl daemon-reload
