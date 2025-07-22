9dac080cac9bbb6ccdf44b0b41633a3b38d27a0d
CHendrick00 <chandlerhendrick@hotmail.com>
2025-07-21 19:37:58 -0500

    heartbeat script fixes

--- heartbeat/heartbeat.py
+++ heartbeat/heartbeat.py
@@ -19,36 +19,38 @@ maxTimeouts = int(config['maxTimeouts'])
 
 plugError = False
 
-async def kasa_setup(ip):
+async def kasa_setup(name, ip):
     global plugError
     try:
         plug = SmartPlug(ip)
         await plug.update()
         if plugError is True:
-            webhook = DiscordWebhook(url=whurl, content="ATTN: Cooling Pump plug reconnected.")
+            webhook = DiscordWebhook(url=whurl, content=f"ATTN: {name} plug reconnected.")
             plugError = False
         return plug
     except:
         if plugError is False:
-            webhook = DiscordWebhook(url=whurl, content="ATTN: Unable to connect to Cooling Pump plug.")
+            webhook = DiscordWebhook(url=whurl, content=f"ATTN: Unable to connect to {name} plug.")
             response = webhook.execute()
             plugError = True
 
-async def toggle_plug(str):
+async def toggle_plug(plug, name, state):
     global plugError
     try:
-        if str == "on" or str == "On" or str == "ON":
+        if state == "on" or state == "On" or state == "ON" and plug.is_off:
             await plug.turn_on()
-        elif str == "off" or str == "Off" or str == "OFF":
+            print(f"{name} plug turned on.")
+        elif state == "off" or state == "Off" or state == "OFF" and plug.is_on:
             await plug.turn_off()
+            print(f"{name} plug turned off.")
         else:
             print("Error: Invalid state")
         if plugError is True:
-            webhook = DiscordWebhook(url=whurl, content="ATTN: Cooling Pump plug reconnected.")
+            webhook = DiscordWebhook(url=whurl, content=f"ATTN: {name} plug reconnected.")
             plugError = False
     except:
         if plugError is False:
-            webhook = DiscordWebhook(url=whurl, content="ATTN: Unable to connect to Cooling Pump plug.") #Message can be changed if desired
+            webhook = DiscordWebhook(url=whurl, content=f"ATTN: Unable to connect to {name} plug.") #Message can be changed if desired
             response = webhook.execute()
             plugError = True
 
@@ -63,9 +65,9 @@ async def main():
     count = 0
     services = {}
     if(pump_ip != ""):
-        pumpPlug = await kasa_setup(pump_ip)
+        pumpPlug = await kasa_setup("Cooling Pump", pump_ip)
     if(cooler_ip != ""):
-        coolerPlug = await kasa_setup(cooler_ip)
+        coolerPlug = await kasa_setup("Cooling System", cooler_ip)
 
     while True:
         try:
@@ -76,17 +78,19 @@ async def main():
             if output == "" and offline == False:
                 count+=1
                 if count >= maxTimeouts:
+                    print("Pi is offline. Disabling cooling system.")
                     webhook = DiscordWebhook(url=whurl, content="Pi is offline. Disabling cooling system.")
                     response = webhook.execute()
-                    await pumpPlug.turn_off()
-                    await coolerPlug.turn_off()
+                    await toggle_plug(pumpPlug, "Cooling Pump", "off")
+                    await toggle_plug(coolerPlug, "Cooling System", "off")
                     offline = True
             # Check sensor statuses
-            else:
+            elif output != "":
                 if offline == True:
                     webhook = DiscordWebhook(url=whurl, content="Pi is back online.")
                     offline = False
                     count = 0
+                    
                 lines = output.split('\n')
                 regex = r'[" "]+'
                 for line in range(len(lines)):
@@ -107,17 +111,19 @@ async def main():
                             webhook = DiscordWebhook(url=whurl, content="%s has stopped running. Status: %s" % (svc, services[svc][0]))
                             response = webhook.execute()
                             # Turn off any relevant plugs
-                            if 'HL' in svc and pumpPlug.is_on:
-                                await pumpPlug.turn_off()
+                            if '-HL' in svc and pumpPlug.is_on:
+                                await toggle_plug(pumpPlug, "Cooling Pump", "off")
                             elif 'DS18B20' in svc and coolerPlug.is_on:
-                                await coolerPlug.turn_off()
+                                await toggle_plug(coolerPlug, "Cooling System", "off")
                     # Reset alerts if sensor comes back online
-                    elif services[svc][0] == "running" and services[svc][1] == True:
-                        webhook = DiscordWebhook(url=whurl, content="%s has come back online." % svc)
-                        response = webhook.execute()
-                        services[svc][1] = False
+                    elif services[svc][0] == "running":
+                        if services[svc][1] == True:
+                            webhook = DiscordWebhook(url=whurl, content="%s has come back online." % svc)
+                            response = webhook.execute()
+                            services[svc][1] = False
                         services[svc][2] = 0
 
+            print()
             time.sleep(interval)
         except KeyboardInterrupt:
             sys.exit(0)
